9eddaa9b2015a9eba0d568993893525b2122ed7d
IT21378270 <it21378270@my.sliit.lk>
2023-04-06 08:27:26 +0530

    first commit

---
+++ src/components/Value/Value.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {
+    Accordion,
+    AccordionItem,
+    AcoordionItemHeading,
+    AcoordionItemButton,
+    AccordionItemPanel,
+    AccordionItemState
+} from 'react-accessible-accordion'
+import "react-accessible-accordion/dist/fancy-example.css"
+import {MdOutlineArrowDropDown} from 'react-icons/md'
+import './Value.css'
+
+const Value = () => {
+  return (
+    <div>
+      value
+    </div>
+  )
+}
+
+export default Value
